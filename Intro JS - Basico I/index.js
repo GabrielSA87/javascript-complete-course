387f2442d28a6dfd26445bcdda4788d6e91994bf
Gabriel Araujo <72812005+GabrielSA87@users.noreply.github.com>
2020-12-22 12:32:52 -0300

    Update index.js

--- Intro JS - Basico I/index.js
+++ Intro JS - Basico I/index.js
@@ -106,7 +106,7 @@ let resultado = calc(1, 2, "+");
 console.log(resultado);
 
 
-/* --- ARROW FUNTION --- */
+/* --- ARROW FUNCTION --- */
 
 let calc2 = (x1, x2, operator) => {
     return eval(`${x1} ${operator} ${x2}`);
